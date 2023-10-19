bca088889f1b706b99fa0ff08a71575009ef3117
AlanGacihi <alanmamicha@gmail.com>
2023-10-19 10:30:58 +0300

    Add

--- main.c
+++ main.c
@@ -4,119 +4,106 @@
 
 #define BUFSIZE 256
 
-typedef struct item {
-    char *word;
-    int weight;
-} Item;
-
-// Function to compare items by weight for sorting
-int compareItems(const void *a, const void *b) {
-    return ((Item *)b)->weight - ((Item *)a)->weight;
-}
+typedef struct item{
+  char *word;
+  int weight;
+}Item;
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <knowledge_base_file> <query_list_file>\n", argv[0]);
-        return 1;
-    }
-
-    char *dictionaryFilePath = argv[1];
-    char *queryFilePath = argv[2];
+    char *dictionaryFilePath = argv[1]; //this keeps the path to dictionary file
+    char *queryFilePath = argv[2]; //this keeps the path to the file that keeps a list of query wrods, 1 query per line
+    int wordCount=0; //this variable will keep a count of words in the dictionary, telling us how much memory to allocate
+    int queryCount=0; //this variable will keep a count of queries in the query file, telling us how much memory to allocate for the query words
     
-    int wordCount = 0;
-    int queryCount = 0;
-
-    // Read the knowledge base file
+    ////////////////////////////////////////////////////////////////////////
+    ///////////////////////// read dictionary file /////////////////////////
+    ////////////////////////////////////////////////////////////////////////
     FILE *fp = fopen(dictionaryFilePath, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Error opening file: %s\n", dictionaryFilePath);
+    char *line = NULL; //variable to be used for line counting
+    size_t lineBuffSize = 0; //variable to be used for line counting
+    ssize_t lineSize; //variable to be used for line counting
+    
+    //check if the file is accessible, just to make sure...
+    if(fp == NULL){
+        fprintf(stderr, "Error opening file:%s\n",dictionaryFilePath);
         return -1;
     }
 
-    char word[BUFSIZE];
-    int weight;
-    while (fscanf(fp, "%s %d\n", word, &weight) == 2) {
+    //First, let's count number of lines. This will help us know how much memory to allocate
+    while((lineSize = getline(&line,&lineBuffSize,fp)) !=-1)
+    {
         wordCount++;
     }
 
-    // Allocate memory for dictionary items
-    Item *dictionary = (Item *)malloc(wordCount * sizeof(Item));
-    if (dictionary == NULL) {
-        fprintf(stderr, "Memory allocation error.\n");
-        return -1;
+    
+    // Allocate memory for your data structure, by the size of "wordCount"
+    Item* items = (Item*)malloc(sizeof(Item)*wordCount);
+    
+    //Read the file once more, this time to fill in the data into memory
+    fseek(fp, 0, SEEK_SET);// rewind to the beginning of the file, before reading it line by line.
+    char word[BUFSIZE]; //to be used for reading lines in the loop below
+    int weight;
+    for(int i = 0; i < wordCount; i++)
+    {
+        fscanf(fp, "%s %d\n",word,&weight);
+        
+        // Store the dictionary words into your data structure
+        items[i].word = (char*)malloc(sizeof(char)*strlen(word));
     }
-
-    // Read the knowledge base file again and store data
-    fseek(fp, 0, SEEK_SET);
-    for (int i = 0; i < wordCount; i++) {
-        fscanf(fp, "%s %d\n", word, &weight);
-        dictionary[i].word = strdup(word);
-        dictionary[i].weight = weight;
-    }
+    //close the input file
     fclose(fp);
 
-    // Read the query list file
+    ////////////////////////////////////////////////////////////////////////
+    ///////////////////////// read query list file /////////////////////////
+    ////////////////////////////////////////////////////////////////////////
     fp = fopen(queryFilePath, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "Error opening file: %s\n", queryFilePath);
+        
+    //check if the file is accessible, just to make sure...
+    if(fp == NULL){
+        fprintf(stderr, "Error opening file:%s\n",queryFilePath);
         return -1;
     }
 
-    char *line = NULL;
-    size_t lineBuffSize = 0;
-    ssize_t lineSize;
-
-    while ((lineSize = getline(&line, &lineBuffSize, fp) != -1)) {
+    //First, let's count number of queries. This will help us know how much memory to allocate
+    while((lineSize = getline(&line,&lineBuffSize,fp)) !=-1)
+    {
         queryCount++;
     }
-    free(line);
+    free(line); //getline internally allocates memory, so we need to free it here so as not to leak memory!!
 
-    // Allocate memory for query words
-    char **queryWords = (char **)malloc(queryCount * sizeof(char *));
-    if (queryWords == NULL) {
-        fprintf(stderr, "Memory allocation error.\n");
-        return -1;
-    }
 
-    fseek(fp, 0, SEEK_SET);
-    for (int i = 0; i < queryCount; i++) {
-        getline(&queryWords[i], &lineBuffSize, fp);
-        // Remove newline character from the query word
-        queryWords[i][strcspn(queryWords[i], "\n")] = '\0';
+    
+    // Allocate memory for storing query words, by the size of "queryCount"
+    char** queries = (char**)malloc(sizeof(char*)*queryCount);
+    
+
+    fseek(fp, 0, SEEK_SET);// rewind to the beginning of the file, before reading it line by line.
+    for(int i = 0; i < queryCount; i++)
+    {
+        fscanf(fp, "%s\n",word);
+
+        // Store the query words in a list like data structure 
+        queries[i] = (char*)malloc(sizeof(char)*strlen(word));  
     }
+    //close the input file
     fclose(fp);
 
-    // Process queries
-    for (int i = 0; i < queryCount; i++) {
-        printf("Query word:%s\n", queryWords[i]);
-        
-        int suggestionsCount = 0;
-        for (int j = 0; j < wordCount; j++) {
-            if (strncmp(dictionary[j].word, queryWords[i], strlen(queryWords[i])) == 0) {
-                printf("%s %d\n", dictionary[j].word, dictionary[j].weight);
-                suggestionsCount++;
-
-                if (suggestionsCount == 10) {
-                    break;  // Limit to the top 10 suggestions
-                }
-            }
-        }
-
-        if (suggestionsCount == 0) {
-            printf("No suggestion!\n");
-        }
-    }
-
-    // Free allocated memory
-    for (int i = 0; i < queryCount; i++) {
-        free(queryWords[i]);
-    }
-    free(queryWords);
-
-    for (int i = 0; i < wordCount; i++) {
-        free(dictionary[i].word);
-    }
-    free(dictionary);
-
+    ////////////////////////////////////////////////////////////////////////
+    ///////////////////////// reading input is done ////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+    
+    //Now it is your turn to do the magic!!!
+    //do search/sort/print, whatever you think you need to do to satisfy the requirements of the assignment!
+    //loop through the query words and list suggestions for each query word if there are any
+    //don't forget to free the memory before you quit the program!
+    
+    
+    //OUTPUT SPECS:
+    // use the following if no word to suggest: printf("No suggestion!\n");
+    // use the following to print a single line of outputs (assuming that the word and weight are stored in variables named word and weight, respectively): 
+    // printf("%s %d\n",word,weight);
+    // if there are more than 10 outputs to print, you should print the top 10 weighted outputs.
+    
+    printf("Done\n");
     return 0;
 }
